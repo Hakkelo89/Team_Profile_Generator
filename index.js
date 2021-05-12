d7b2e3314380cea4b4da03cee1b1d957f4ac9705
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-05-12 20:31:03 +0100

    lib file created and js files are saved in lib file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const inquirer = require("inquirer");
 
-const Manager = require("./utils/constructor/Manager");
-const Employee = require("./utils/constructor/Employee");
-const Engineer = require("./utils/constructor/Engineer");
-const Intern = require("./utils/constructor/Intern");
+const Manager = require("./utils/lib/Manager");
+const Employee = require("./utils/lib/Employee");
+const Engineer = require("./utils/lib/Engineer");
+const Intern = require("./utils/lib/Intern");
 
 const getAnswers = async (questions) => {
   const answers = await inquirer.prompt(questions);
